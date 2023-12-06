fa17593ea858e9f725b47ead5b1ce1eb0a71d079
Klaasjanssen <jelle10101@gmail.com>
2023-12-06 09:47:16 +0100

    D6 math instead of bruteforce

--- D6P2_boat_race.py
+++ D6P2_boat_race.py
@@ -1,3 +1,5 @@
+import math
+
 with open("data/D6.txt", "r") as f:
 #with open("test.txt", "r") as f:
     input = [x.strip("\n") for x in f.readlines()]
@@ -5,9 +7,14 @@ with open("data/D6.txt", "r") as f:
 time = int("".join([x for x in input[0].split(" ") if not x == ""][1:]))
 distance = int("".join([x for x in input[1].split(" ") if not x == ""][1:]))
 
-beat = 0
-for hold in range(time):
-    if (time - hold) * hold > distance:
-        beat += 1
+# c = distance
+# b = time
+# x = time holding the button
+# c = (b - x) * x
+# c = bx - x**2
+# x**2 - bx + c = 0
 
-print(beat)
+t1 = int((time + math.sqrt(time**2 - 4*distance)) / 2)
+t2 = int((time - math.sqrt(time**2 - 4*distance)) / 2)
+
+print(abs(t2 - t1))
